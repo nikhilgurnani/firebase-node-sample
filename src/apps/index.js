5f6f4ba1af09d743cd46366893c6d382d5337075
Nikhil Gurnani <nikhil@grappus.com>
2018-12-22 17:44:21 +0530

    Added index file, parser and morgan

---
+++ src/apps/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    morgan: require('./morgan'),
+};
---
+++ src/parsers/fourColonParser.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = (combinedString) => {
+    return combinedString.split('::::');
+};
